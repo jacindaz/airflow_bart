916189fc4f99f7649a35d4e5a2363735b5859428
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2018-08-26 21:04:17 -0700

    Creates a bart_station_names table using sqlalchemy

--- dags/process_csv.py
+++ dags/process_csv.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import ipdb
+import os
 import xlrd
 
 from airflow import DAG
@@ -7,6 +8,9 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
 
 from openpyxl import load_workbook
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext import declarative
 
 default_args = {
     'owner': 'me',
@@ -15,6 +19,19 @@ default_args = {
     'retry_delay': dt.timedelta(minutes=5),
 }
 
+class Base(declarative.declarative_base()):
+    __abstract__ = True
+
+class StationName(Base):
+    __tablename__ = 'bart_station_names'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    two_letter_code = Column(String)
+
+    def __repr__(self):
+        return "<StationName(id='%s', name='%s', two_letter_code='%s')>" % (
+            self.id, self.name, self.two_letter_code)
 
 def process_station_names():
     book = xlrd.open_workbook("/Users/jacinda.zhong/Downloads/Station_Names.xls")
@@ -29,8 +46,9 @@ def process_station_names():
         row_values.pop(0)
         data.append(row_values)
 
-    print(data)
-
+    engine = create_engine('postgresql+psycopg2://jacinda.zhong@localhost:5432/sf_bart')
+    StationName.__table__
+    Base.metadata.create_all(engine)
 
 with DAG('process_file',
     default_args = default_args,
