d013b79f230482678e7a58cf5c559040f46f19fc
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2018-10-21 12:43:40 -0700

    DAG/task to create table for an hourly ridership file

---
+++ dags/hourly_ridership_by_origin_destination_pairs.py
@@ -0,0 +1,67 @@
+import csv
+import datetime as dt
+import ipdb
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from openpyxl import load_workbook
+from sqlalchemy import create_engine, MetaData, Table, Column, Date, Integer, String
+
+import helpers.constants as constants
+
+default_args = {
+    'owner': 'me',
+    'start_date': dt.datetime(2018, 6, 27),
+    'retries': 0,
+    'retry_delay': dt.timedelta(minutes=5),
+}
+
+DB_URI = 'postgresql+psycopg2://jacinda.zhong@localhost:5432/sf_data'
+FILE_PATH = 'data/bart/hourly_ridership_by_origin_destination_pairs.csv'
+TABLE_NAME = 'fact_hourly_ridership_2011'
+
+
+def create_table():
+    engine = create_engine(DB_URI)
+    engine.execute('CREATE SCHEMA IF NOT EXISTS "bart"')
+
+    # Day, Hour, Origin Station, Destination Station, and Trip Count
+    meta = MetaData(engine, schema="bart")
+    table = Table(TABLE_NAME, meta,
+                     Column('id', Integer, primary_key=True),
+                     Column('date', Date),
+                     Column('hour', Integer),
+                     Column('origin_station', String),
+                     Column('destination_station', String),
+                     Column('ridership', Integer)
+                 )
+    meta.create_all()
+
+
+def import_hourly_ridership():
+    with open(FILE_PATH) as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+
+            ipdb.set_trace()
+
+
+dag = DAG('hourly_ridership_origin_dest_pairs',
+          default_args=default_args,
+          schedule_interval='@hourly',
+      )
+
+create_table = PythonOperator(
+                             task_id='create_table',
+                             python_callable=create_table,
+                             dag=dag
+                         )
+
+import_hourly_ridership = PythonOperator(
+                        task_id='import_hourly_ridership',
+                        python_callable=import_hourly_ridership,
+                        dag=dag
+                    )
+
+import_hourly_ridership.set_upstream(create_table)
