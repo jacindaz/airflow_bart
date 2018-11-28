42505daa2a4bfb5437503c2ffc63980860887c3f
Jacinda Zhong <jacinda.zhong@cloverhealth.com>
2018-11-27 20:14:50 -0500

    Test annual ridership years are correct

--- tests/test_bart_annual_ridership_exits.py
+++ tests/test_bart_annual_ridership_exits.py
@@ -2,6 +2,7 @@ import xlrd
 
 import dags.bart_annual_ridership_exits as dag
 
+
 def test_create_table(postgresql_db):
     dag.create_table(postgresql_db.postgresql_url)
 
@@ -23,3 +24,19 @@ def test_import_annual_ridership(postgresql_db):
     expected_count = sheet.nrows - 6
 
     assert result_count == expected_count
+
+def test_import_annual_ridership_years(postgresql_db):
+    dag.create_table(postgresql_db.postgresql_url)
+
+    test_file_path = 'tests/test_data/test_BART_Ridership_FY73_FY18.xlsx'
+    dag.import_annual_ridership(db_uri=postgresql_db.postgresql_url, file_path=test_file_path)
+
+    result_proxy_obj1 = postgresql_db.session.execute(f"select max(year) from bart.{dag.TABLE_NAME}")
+    max_year = result_proxy_obj1.first()[0]
+
+    assert max_year == 2018
+
+    result_proxy_obj2 = postgresql_db.session.execute(f"select min(year) from bart.{dag.TABLE_NAME}")
+    min_year = result_proxy_obj2.first()[0]
+
+    assert min_year == 1973
